29155a20b15fabf30ac0017b6677bf614dceda80
rue-22 <cssalces@up.edu.ph>
2025-04-29 16:43:24 +0800

    style: remove comments

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
 	import '../app.css';
-	// import { DarkMode } from 'flowbite-svelte';
 	import Header from '$lib/components/Header.svelte';
 
 	const { children } = $props();
 </script>
 
-<!-- <DarkMode /> -->
 <div class="grid min-h-screen grid-rows-[auto_1fr]">
 	<Header />
 
