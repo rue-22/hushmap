8579f0306dcd7bf328e2efd7454bbf2182ef52e6
rue-22 <cssalces@up.edu.ph>
2025-04-29 16:42:32 +0800

    feat: add Circle and Property components

---
+++ src/lib/components/Circle.svelte
@@ -0,0 +1,24 @@
+<script>
+	const { color, dbaLevel } = $props();
+</script>
+
+<div
+	class="text-{color} border-{color} center flex size-10 cursor-pointer flex-col items-center justify-center gap-0 rounded-full border-3 bg-white"
+>
+	{#if color === 'quiet-green'}
+		<span class="icon-[solar--volume-small-linear] size-xl"></span>
+	{:else if color === 'loud-orange'}
+		<span class="icon-[solar--volume-loud-linear]"></span>
+	{:else}
+		<span class="icon-[solar--volume-loud-bold]"></span>
+	{/if}
+
+	<p class="text-xs">{dbaLevel}</p>
+</div>
+
+<style>
+	span {
+		width: 24px;
+		height: 24px;
+	}
+</style>
---
+++ src/lib/components/Property.svelte
@@ -0,0 +1,29 @@
+<script>
+	const props = $props();
+
+	let dbClass = $state('');
+	let dbText = $state('');
+	if (props.attr === 'dBA Level')
+		if (props.val < 60) {
+			dbClass = 'text-quiet-green';
+			dbText = ' dBA (Quiet)';
+		} else if (props.val > 60 && props.val < 69) {
+			dbClass = 'text-loud-orange';
+			dbText = ' dBA (Loud)';
+		} else {
+			dbClass = 'text-very-loud-red';
+			dbText = ' dBA (Very Loud)';
+		}
+</script>
+
+<div>
+	<p>
+		<span class="italic">{props.attr}</span>:
+		{#if props.attr === 'Timeframe'}
+			{@const [start, end] = props.val}
+			<span class="font-semibold {dbClass}">{start} to {end}</span>
+		{:else}
+			<span class="font-semibold {dbClass}">{props.val + dbText}</span>
+		{/if}
+	</p>
+</div>
