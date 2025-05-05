2f9923f048a08fda8c117321e31d60f5920b1f62
rue-22 <cssalces@up.edu.ph>
2025-05-05 16:01:13 +0800

    style: change colors

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,7 +2,7 @@
 	import Toggle from '$lib/components/Toggle.svelte';
 </script>
 
-<header class="flex justify-between text-center md:px-8 dark:bg-[#202937]">
+<header class="bg-header-color flex justify-between text-center shadow-xl md:px-8">
 	<h1 class="py-2 text-3xl font-bold text-black dark:text-white">HushMap</h1>
 	<Toggle />
 </header>
