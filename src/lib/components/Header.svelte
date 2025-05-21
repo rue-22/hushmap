556367fa75b173e024d69db6991f3f0a1ad8f9b3
carlsalces <cssalces@up.edu.ph>
2025-05-22 01:07:31 +0800

    feat: add favicon and logo in header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,8 +1,12 @@
 <script>
 	import Toggle from '$lib/components/Toggle.svelte';
+	import favicon from '$lib/static/logo.png';
 </script>
 
-<header class="bg-header-color flex justify-between px-4 py-2 text-center shadow-xl md:px-8">
-	<a class="text-3xl font-bold text-white" href="/">HushMap</a>
+<header class="bg-header-color flex justify-between px-4 pt-2 py-3 text-center shadow-xl md:px-8">
+	<a class="flex w-8 aspect-square items-center gap-2" href="/">
+		<img src={favicon} alt="HushMap's logo" />
+		<h1 class="text-3xl font-bold text-white">HushMap</h1>
+	</a>
 	<Toggle />
 </header>
