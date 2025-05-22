d6664e1318c1a0f8ddbc1afeb4ffee849baa3e66
carlsalces <cssalces@up.edu.ph>
2025-05-22 17:29:55 +0800

    feat: display unmeasured points

--- src/lib/components/Circle.svelte
+++ src/lib/components/Circle.svelte
@@ -1,19 +1,32 @@
 <script>
 	const { color, dbaLevel } = $props();
+
+	let tbColor = $state("");
+	if (color === 'quiet-green') tbColor = "text-quiet-green border-quiet-green"
+	else if (color === 'loud-orange') tbColor = "text-loud-orange border-loud-orange"
+	else if (color === 'very-loud-red') tbColor = "text-very-loud-red border-very-loud-red"
+	else tbColor = `text-gray-300 border-gray-300`
 </script>
 
 <div
-	class="text-{color} border-{color} center flex size-10 cursor-pointer flex-col items-center justify-center gap-0 rounded-full border-2 bg-header-color"
+	class="{tbColor} center flex size-10 cursor-pointer flex-col items-center justify-center gap-0 rounded-full border-2 bg-header-color"
 >
 	{#if color === 'quiet-green'}
 		<span class="icon-[solar--volume-small-linear] size-xl"></span>
 	{:else if color === 'loud-orange'}
 		<span class="icon-[solar--volume-loud-linear]"></span>
-	{:else}
+	{:else if color === 'very-loud-red'}
 		<span class="icon-[solar--volume-loud-bold]"></span>
+	{:else}
+		<span class="icon-[solar--muted-bold]"></span>
 	{/if}
 
-	<p class="text-xs">{dbaLevel}</p>
+	{#if dbaLevel === 0}
+		<p class="text-xs">N/A</p>
+	{:else}
+		<p class="text-xs">{dbaLevel}</p>
+	{/if}
+
 </div>
 
 <style>
